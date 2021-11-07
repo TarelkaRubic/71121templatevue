a67df319a3563c8edf0b4e140ff50e068f983c87
TarelkaRubic <dborborisov@gmail.com>
2021-11-07 17:55:21 +0300

    изменили profile

--- src/router/index.js
+++ src/router/index.js
@@ -44,6 +44,21 @@ export const constantRoutes = [
   },
 
   {
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    hidden: true,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/profile/index'),
+        name: 'Profile',
+        meta: { title: 'Profile', icon: 'user', noCache: true }
+      }
+    ]
+  },
+
+  {
     path: '/',
     component: Layout,
     redirect: '/dashboard',
@@ -170,7 +185,7 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', 
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
